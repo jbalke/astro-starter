9ac51a5d27952d82fadb6146e086196b3f83e1cc
John Balke <john.balke@gmail.com>
2024-12-01 20:12:42 +0000

    upgrade tailwindcss 4 beta

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,22 +1,14 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
-import tailwind from "@astrojs/tailwind";
-
 import react from "@astrojs/react";
-
 import sitemap from "@astrojs/sitemap";
-
 import mdx from "@astrojs/mdx";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [
-		tailwind({
-			applyBaseStyles: false,
-		}),
-		react(),
-		sitemap(),
-		mdx(),
-	],
+	integrations: [react(), sitemap(), mdx()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
